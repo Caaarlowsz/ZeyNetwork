bf594c9b99af6942ce6f9e49eb8b388283d884df
Caaarlowsz <caaarlowsz@outlook.com>
2025-07-06 19:25:46 -0300

    refactor: Remove unused kits from ListKits enum
    Deletes several unused kit entries from the ListKits enumeration.

--- kitpvp/src/main/java/Zey/PvP/Essencial/ListKits.java
+++ kitpvp/src/main/java/Zey/PvP/Essencial/ListKits.java
@@ -1,9 +1,6 @@
 package Zey.PvP.Essencial;
 
 public enum ListKits {
-
-	boxer, burstmaster, camel, deshfire, deshviper, fisherman, forcefield, gladiator, hulk, kangaroo, madman, ninja,
-	phantom, poseidon, reaper, ryu, seya, snail, sonic, stomper, thor, turtle, viking, viper, ajnin, thresh, switcher,
-	monk
-
+	camel, deshfire, fisherman, forcefield, gladiator, hulk, kangaroo, madman, ninja, poseidon, snail, sonic, stomper,
+	thor, turtle, viking, viper, ajnin, thresh, switcher, monk
 }
