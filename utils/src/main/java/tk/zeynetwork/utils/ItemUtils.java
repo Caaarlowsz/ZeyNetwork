e97753fefc25b22ed8586c97012745068abe84ba
Caaarlowsz <caaarlowsz@outlook.com>
2025-07-10 18:21:10 -0300

    feat: Add methods for creating colored leather armor
    Introduces new static utility methods in the 'ItemUtils' class to simplify
    the creation and coloring of leather armor.

--- utils/src/main/java/tk/zeynetwork/utils/ItemUtils.java
+++ utils/src/main/java/tk/zeynetwork/utils/ItemUtils.java
@@ -2,9 +2,11 @@ package tk.zeynetwork.utils;
 
 import java.util.Arrays;
 
+import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
 
 public class ItemUtils {
 
@@ -33,6 +35,38 @@ public class ItemUtils {
 		ItemMeta mItem = item.getItemMeta();
 		mItem.setDisplayName(display);
 		mItem.setLore(Arrays.asList(lore));
+		item.setItemMeta(mItem);
+		return item;
+	}
+
+	public static ItemStack leatherArmor(Material type, Color color) {
+		return leatherArmor(type, color, 1);
+	}
+
+	public static ItemStack leatherArmor(Material type, Color color, int amount) {
+		return leatherArmor(type, color, amount, 0);
+	}
+
+	public static ItemStack leatherArmor(Material type, Color color, int amount, int durability) {
+		return colorArmor(item(type, amount, durability), color);
+	}
+
+	public static ItemStack leatherArmor(Material type, Color color, String display, String... lore) {
+		return leatherArmor(type, color, 1, display, lore);
+	}
+
+	public static ItemStack leatherArmor(Material type, Color color, int amount, String display, String... lore) {
+		return leatherArmor(type, color, amount, 0, display, lore);
+	}
+
+	public static ItemStack leatherArmor(Material type, Color color, int amount, int durability, String display,
+			String... lore) {
+		return colorArmor(item(type, amount, durability, display, lore), color);
+	}
+
+	private static ItemStack colorArmor(ItemStack item, Color color) {
+		LeatherArmorMeta mItem = (LeatherArmorMeta) item.getItemMeta();
+		mItem.setColor(color);
 		item.setItemMeta(mItem);
 		return item;
 	}
