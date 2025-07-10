89137beb7e16039679d4e83de5671eb5add63267
Caaarlowsz <caaarlowsz@outlook.com>
2025-07-10 18:52:37 -0300

    refactor: Use ItemUtils for enchanting PvP kit swords
    Refactored the kit application logic in 'Kit PvP' to leverage the
    newly created ItemUtils.enchantItem method.

--- kitpvp/src/main/java/tk/zeynetwork/kitpvp/kits/PvP.java
+++ kitpvp/src/main/java/tk/zeynetwork/kitpvp/kits/PvP.java
@@ -3,7 +3,6 @@ package tk.zeynetwork.kitpvp.kits;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 
 import tk.zeynetwork.kitpvp.api.Kit;
 import tk.zeynetwork.utils.ItemUtils;
@@ -17,8 +16,7 @@ public final class PvP extends Kit {
 	@Override
 	public void giveItems(Player player) {
 		super.giveItems(player);
-		ItemStack sword = ItemUtils.item(Material.STONE_SWORD, "§e§lESPADA");
-		sword.addEnchantment(Enchantment.DAMAGE_ALL, 1);
-		player.getInventory().setItem(0, sword);
+		player.getInventory().setItem(0,
+				ItemUtils.enchantedItem(Material.STONE_SWORD, Enchantment.DAMAGE_ALL, 1, "§e§lESPADA"));
 	}
 }
